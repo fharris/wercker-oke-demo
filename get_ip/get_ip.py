3911e2db2a36de3d4794019bffcb202086ca47dc
fharris <fharris@FHARRIS-ES.pt.oracle.com>
2018-12-10 10:34:28 +0100

    teste para INCM

--- get_ip/get_ip.py
+++ get_ip/get_ip.py
@@ -15,9 +15,9 @@ def get_ip():
 def mgg():
     return json.dumps({"message": "Python service response for the meetup!!!! !!"})
 
-@app.route('/smoketest')
+@app.route('/casadamoeda')
 def smoketest():
-    return json.dumps({"message": "smoke test ok for casa da moeda"})
+    return json.dumps({"message": "Olá casa da moeda"})
 
 if __name__ == "__main__":
 
